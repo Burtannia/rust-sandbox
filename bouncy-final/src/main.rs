8e54ca0906913e2d100147b229992efcbfd08184
James Burton <jburton@sopost.com>
2021-05-18 14:15:25 +0100

    Final iteration of the bouncy ball

---
+++ bouncy-final/src/main.rs
@@ -0,0 +1,114 @@
+use pancurses::{Window, Input, initscr, endwin};
+
+enum VertDir {
+    Up,
+    Down
+}
+
+enum HorizDir {
+    Left,
+    Right
+}
+
+struct Ball {
+    x: u32,
+    y: u32,
+    vert_dir: VertDir,
+    horiz_dir: HorizDir
+}
+
+struct Frame {
+    width: u32,
+    height: u32
+}
+
+struct Game {
+    frame: Frame,
+    ball: Ball
+}
+
+impl Game {
+    fn new(window: &Window) -> Result<Game, String> {
+
+        let frame = match window.get_max_yx() {
+            (y, x) if x < 10 || y < 10 => Err("Dimensions too small"),
+            (y, x) => Ok(Frame {
+                width: x as u32 - 2,
+                height: y as u32 - 2
+            })
+        }?;
+
+        let ball = Ball {
+            x: 2,
+            y: 4,
+            vert_dir: VertDir::Up,
+            horiz_dir: HorizDir::Left
+        };
+
+        Ok(Game {frame, ball})
+    }
+
+    fn step(&mut self) {
+        self.ball.bounce(&self.frame);
+        self.ball.mv();
+    }
+}
+
+impl Ball {
+    fn bounce(&mut self, frame: &Frame) {
+        if self.x == 0 {
+            self.horiz_dir = HorizDir::Right
+        } else if self.x == frame.width - 1 {
+            self.horiz_dir = HorizDir::Left
+        }
+
+        if self.y == 0 {
+            self.vert_dir = VertDir::Up
+        } else if self.y == frame.height - 1 {
+            self.vert_dir = VertDir::Down
+        }
+    }
+
+    fn mv(&mut self) {
+        match self.horiz_dir {
+            HorizDir::Left => self.x -= 1,
+            HorizDir::Right => self.x += 1
+        }
+        match self.vert_dir {
+            VertDir::Up => self.y += 1,
+            VertDir::Down => self.y -= 1
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let window = initscr();
+
+    window.timeout(33);
+
+    let mut game = Game::new(&window)?;
+
+    loop {
+        window.clear();
+        window.border('|', '|', '-', '-', '+', '+', '+', '+');
+        window.mvaddch(game.ball.y as i32 + 1, game.ball.x as i32 + 1, 'o'); // + 1 is for the border
+        window.mv(0,0); // move cursor out of the way
+        window.refresh();
+
+        match window.getch() {
+            Some(Input::Character('q')) => {
+                endwin();
+                println!("Thanks for playing!");
+                return Ok(());
+            }
+
+            Some(Input::KeyResize) => {
+                game = Game::new(&window)?
+            }
+
+            _ => {
+                game.step();
+            }
+        }
+    }
+}
